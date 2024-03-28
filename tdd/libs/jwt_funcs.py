89e7a7c3097cc6884a01c306831c5f4b9d527a0a
Sergio <sergio@Ginhu>
2024-03-28 14:38:52 -0300

    refatoração de testes, retirando mock e correção da verificação de token expirado

--- tdd/libs/jwt_funcs.py
+++ tdd/libs/jwt_funcs.py
@@ -4,21 +4,21 @@ from jwt import InvalidSignatureError
 
 from tdd.erros import TokenInvalido, ImpossivelVerificarTimeout, JwtExpirado
 
-key = '1key'
+key = 'key'
 
 
-def gera_jwt(dados: dict, dh: bool = False) -> str:
+def gera_jwt(dados: dict, dh=False, chave=key) -> str:
     dicionario = dict(dados)
     if dh:
         dicionario['criado_em'] = datetime.now().astimezone().isoformat()
 
-    token = jwt.encode(dicionario, key, algorithm='HS256')
+    token = jwt.encode(dicionario, chave, algorithm='HS256')
     return token
 
 
-def descriptografa_jwt(token: str, expira_em: int = 0) -> dict:
+def descriptografa_jwt(token: str, expira_em=0, chave=key) -> dict:
     try:
-        descript_jwt = jwt.decode(token, key, algorithms='HS256')
+        descript_jwt = jwt.decode(token, chave, algorithms='HS256')
     except InvalidSignatureError:
         raise TokenInvalido('Token fornecido não é válido')
 
@@ -31,6 +31,7 @@ def descriptografa_jwt(token: str, expira_em: int = 0) -> dict:
     datetime_agora = datetime.now().astimezone()
     criado_em = datetime.fromisoformat(descript_jwt['criado_em'])
 
-    if (datetime_agora - criado_em).seconds <= expira_em:
+    if (datetime_agora - criado_em).seconds + 1 < expira_em:
         return descript_jwt
+
     raise JwtExpirado('Tempo de uso do link expirado, favor gerar um novo')
--- tests/libs/test_jwt_funcs.py
+++ tests/libs/test_jwt_funcs.py
@@ -1,31 +1,28 @@
 from datetime import datetime
 import time
-import jwt
 import pytest
-from pytest_mock import MockerFixture
 from tdd.erros import ImpossivelVerificarTimeout, JwtExpirado, TokenInvalido
 from tdd.libs import jwt_funcs
 
 
-def test_gera_jwt_sem_dh(meu_dict: dict, mocker: MockerFixture):
+def test_gera_jwt_sem_dh(meu_dict: dict):
     """Testa geração do JWT sem o parâmetro dh"""
-    mocker.patch('tdd.libs.jwt_funcs.key', 'key')
+    # mocker.patch('tdd.libs.jwt_funcs.key', 'key')
 
     token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21lIjoiU2VyZ2lvIiwiaWRhZGUiOjM3fQ.'\
             'JLCj4hpHaxDMmFoI8BqXtV69PhoELkAKmXoDJPXApFQ'
 
-    retorno = jwt_funcs.gera_jwt(meu_dict)
+    retorno = jwt_funcs.gera_jwt(meu_dict, chave='key')
     assert retorno == token
 
 
-def test_gera_jwt_com_dh(meu_dict: dict, mocker: MockerFixture):
+def test_gera_jwt_com_dh(meu_dict: dict):
     """Testa a geração do JWT com o paramêtro dh = True"""
     # Verifica se a chave 'criado_em' existe dentro do dict
-    mocker.patch('tdd.libs.jwt_funcs.key', 'key')
 
     token_retorno = jwt_funcs.gera_jwt(meu_dict, True)
-    dict_jwt_descript = jwt.decode(token_retorno, 'key', algorithms='HS256')
-    assert 'criado_em' in dict_jwt_descript.keys()
+    dict_jwt_descript = jwt_funcs.descriptografa_jwt(token_retorno)
+    assert 'criado_em' in dict_jwt_descript
 
     # Verifica se a diferença entre o datetime.now e o valor da chave 'criado_em'
     data_retorno = datetime.fromisoformat(dict_jwt_descript['criado_em'])
@@ -37,39 +34,36 @@ def test_gera_jwt_com_dh(meu_dict: dict, mocker: MockerFixture):
     assert dict_jwt_descript == meu_dict
 
 
-def test_descriptografa_jwt_expira_em_default(meu_dict, mocker: MockerFixture):
+def test_descriptografa_jwt_expira_em_default(meu_dict):
     """Teste de descriptogra JWT sem passar o parâmetro expira_em"""
-    mocker.patch('tdd.libs.jwt_funcs.key', 'key')
     token = jwt_funcs.gera_jwt(meu_dict)
     retorno_jwt_descript = jwt_funcs.descriptografa_jwt(token)
     assert retorno_jwt_descript == meu_dict
 
 
-def test_descriptografa_jwt_raise_TokenInvalido(meu_dict: dict, mocker: MockerFixture):
+def test_descriptografa_jwt_raise_TokenInvalido(meu_dict: dict):
     """Teste de descriptografar JWT enviando token inválido"""
-    mocker.patch('tdd.libs.jwt_funcs.key', 'key')
     token = jwt_funcs.gera_jwt(meu_dict) + 't'
     with pytest.raises(TokenInvalido) as exinfo:
         jwt_funcs.descriptografa_jwt(token)
     assert str(exinfo.value) == 'Token fornecido não é válido'
 
 
-def test_descriptografa_jwt_raise_ImpossivelVerificarTimeout(meu_dict: dict, mocker: MockerFixture):
+def test_descriptografa_jwt_raise_ImpossivelVerificarTimeout(meu_dict: dict):
     """Teste de descriptografar JWT sem possibilidade de verificar timeout"""
-    mocker.patch('tdd.libs.jwt_funcs.key', 'key')
     token = jwt_funcs.gera_jwt(meu_dict)
     with pytest.raises(ImpossivelVerificarTimeout) as exinfo:
         jwt_funcs.descriptografa_jwt(token, 30)
     assert str(exinfo.value) == 'Não é possível verificar timeout'
 
 
-def test_descriptografa_jwt_raise_JwtExpirado(meu_dict: dict, mocker: MockerFixture):
+def test_descriptografa_jwt_raise_JwtExpirado(meu_dict: dict):
     """Teste de descriptografar JWT com token expirado"""
-    mocker.patch('tdd.libs.jwt_funcs.key', 'key')
+
     token = jwt_funcs.gera_jwt(meu_dict, True)
-    time.sleep(2)
+    time.sleep(0.1)
     with pytest.raises(JwtExpirado) as exinfo:
-        jwt_funcs.descriptografa_jwt(token, 1)
+        jwt_funcs.descriptografa_jwt(token, 0.05)
     assert str(exinfo.value) == 'Tempo de uso do link expirado, favor gerar um novo'
 
 
@@ -78,7 +72,7 @@ def test_descriptografa_jwt_expira_em_60(meu_dict: dict):
     timeout = 60
     token = jwt_funcs.gera_jwt(meu_dict, True)
     retorno = jwt_funcs.descriptografa_jwt(token, timeout)
-    assert 'criado_em' in retorno.keys()
+    assert 'criado_em' in retorno
 
     datetime_atual = datetime.now().astimezone()
     criado_em = datetime.fromisoformat(retorno['criado_em'])
